fdac625e5528484d9fe7c2edaace4d963cfba4eb
Eno Compton <enocom@google.com>
2023-07-10 21:34:36 -0600

    Move equals and hashcode to IT test

--- alloydb-jdbc-connector/src/test/java/com/google/cloud/alloydb/ITConnectorTest.java
+++ alloydb-jdbc-connector/src/test/java/com/google/cloud/alloydb/ITConnectorTest.java
@@ -19,6 +19,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.google.cloud.alloydb.v1beta.AlloyDBAdminClient;
 import com.google.cloud.alloydb.v1beta.InstanceName;
+import com.google.common.base.Objects;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.security.KeyPair;
@@ -30,26 +31,35 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import javax.net.ssl.SSLSocket;
 import org.bouncycastle.operator.OperatorCreationException;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ITConnectorTest {
 
   private String instanceName;
+  private AlloyDBAdminClient alloydbAdminClient;
 
   @Before
-  public void setUp() {
+  public void setUp() throws IOException {
     instanceName = System.getenv("ALLOYDB_INSTANCE_NAME");
+    // Create the client once and close it later.
+    alloydbAdminClient = AlloyDBAdminClient.create();
+  }
+
+  @After
+  public void tearDown() {
+    alloydbAdminClient.close();
   }
 
   @Test
   public void testConnect_createsSocketConnection() throws IOException {
     SSLSocket socket = null;
     ScheduledThreadPoolExecutor executor = null;
-    try (AlloyDBAdminClient alloyDBAdminClient = AlloyDBAdminClientFactory.create()) {
+    try {
       executor = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(2);
       ConnectionInfoRepository connectionInfoRepository =
-          new DefaultConnectionInfoRepository(executor, alloyDBAdminClient);
+          new DefaultConnectionInfoRepository(executor, alloydbAdminClient);
       Connector connector =
           new Connector(
               executor,
@@ -115,5 +125,89 @@ public class ITConnectorTest {
     }
 
     assertThat(stubConnectionInfoCache.hasForceRefreshed()).isTrue();
+  }
+
+  @Test
+  public void testEquals() {
+    ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+    DefaultConnectionInfoRepository connectionInfoRepo =
+        new DefaultConnectionInfoRepository(executor, alloydbAdminClient);
+    KeyPair clientConnectorKeyPair = RsaKeyPairGenerator.generateKeyPair();
+    DefaultConnectionInfoCacheFactory connectionInfoCacheFactory =
+        new DefaultConnectionInfoCacheFactory();
+
+    Connector a =
+        new Connector(
+            executor,
+            connectionInfoRepo,
+            clientConnectorKeyPair,
+            connectionInfoCacheFactory,
+            new ConcurrentHashMap<>());
+
+    assertThat(a)
+        .isNotEqualTo(
+            new Connector(
+                new ScheduledThreadPoolExecutor(1), // Different
+                connectionInfoRepo,
+                clientConnectorKeyPair,
+                connectionInfoCacheFactory,
+                new ConcurrentHashMap<>()));
+
+    assertThat(a)
+        .isNotEqualTo(
+            new Connector(
+                executor,
+                new DefaultConnectionInfoRepository(executor, alloydbAdminClient), // Different
+                clientConnectorKeyPair,
+                connectionInfoCacheFactory,
+                new ConcurrentHashMap<>()));
+
+    assertThat(a)
+        .isNotEqualTo(
+            new Connector(
+                executor,
+                connectionInfoRepo,
+                RsaKeyPairGenerator.generateKeyPair(), // Different
+                connectionInfoCacheFactory,
+                new ConcurrentHashMap<>()));
+
+    assertThat(a)
+        .isNotEqualTo(
+            new Connector(
+                executor,
+                connectionInfoRepo,
+                clientConnectorKeyPair,
+                new DefaultConnectionInfoCacheFactory(),
+                new ConcurrentHashMap<>()));
+  }
+
+  @Test
+  public void testHashCode() {
+    ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+    DefaultConnectionInfoRepository connectionInfoRepo =
+        new DefaultConnectionInfoRepository(executor, alloydbAdminClient);
+    KeyPair clientConnectorKeyPair = RsaKeyPairGenerator.generateKeyPair();
+    DefaultConnectionInfoCacheFactory connectionInfoCacheFactory =
+        new DefaultConnectionInfoCacheFactory();
+    ConcurrentHashMap<InstanceName, ConnectionInfoCache> instances = new ConcurrentHashMap<>();
+
+    Connector a =
+        new Connector(
+            executor,
+            connectionInfoRepo,
+            clientConnectorKeyPair,
+            connectionInfoCacheFactory,
+            instances);
+
+    assertThat(a.hashCode())
+        .isEqualTo(
+            Objects.hashCode(
+                executor,
+                connectionInfoRepo,
+                clientConnectorKeyPair,
+                connectionInfoCacheFactory,
+                instances
+            )
+        );
   }
 }
