65d76e5098a85846ca074d990c2644164603313d
Eno Compton <enocom@google.com>
2025-05-01 14:32:32 -0600

    chore: test Java 21 (#636)
    This matches the official policy:
    https://cloud.google.com/java/docs/supported-java-versions

--- .kokoro/dependencies.sh
+++ .kokoro/dependencies.sh
@@ -38,20 +38,14 @@ function determineMavenOpts() {
       | sed -E 's/^(1\.[0-9]\.0).*$/\1/g'
   )
 
-  if [[ $javaVersion == 17* ]]
-    then
-      # MaxPermSize is no longer supported as of jdk 17
-      echo -n "-Xmx1024m"
-  else
-      echo -n "-Xmx1024m -XX:MaxPermSize=128m"
-  fi
+  echo -n "-Xmx1024m"
 }
 
 export MAVEN_OPTS=$(determineMavenOpts)
 
 # this should run maven enforcer
 retry_with_backoff 3 10 \
-  mvn install -B -V -ntp \
+  ./mvnw install -B -V -ntp \
     -DskipTests=true \
     -Dmaven.javadoc.skip=true \
     -Dclirr.skip=true
